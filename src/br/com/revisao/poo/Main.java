16ea1fc73a050b44c8220063451fbf3a4ccd5cd9
PauloAlves8039 <pj.alves547@gmail.com>
2019-10-26 11:21:46 -0300

    Adicionando pacote com classes para entidade veículo e ponto de entrada

---
+++ src/br/com/revisao/poo/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Arquivo: Classe - Main
+ * Autor: Paulo Alves
+ * Descrição: utilizada como entry point(ponto de entrada) da aplicação para execução das classes no pacote poo
+ * Data: 26/10/2019
+*/
+package br.com.revisao.poo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("***** INFORMAÇÕES DO VEÍCULO *****");
+		VeiculoBase v1 = new VeiculoBase();
+		v1.setMarca("GM");
+		v1.setModelo("Corsa");
+		System.out.println("A marca do veículo é: " + v1.getMarca());
+		System.out.println("O modelo do veículo é: " + v1.getModelo());
+		System.out.println("===== ESTADO DO VEÍCULO =====");
+		v1.desligar();
+		v1.abastecer(15.0f);
+		System.out.println("O veículo " + v1.getModelo() + " está abastecido com " + v1.getQuantidadeCombustivel() + " litros!");
+	}
+}
---
+++ src/br/com/revisao/poo/VeiculoBase.java
@@ -0,0 +1,75 @@
+/*
+ * Arquivo: Classe - VeiculoBase
+ * Autor: Paulo Alves
+ * Descrição: classe de modelo para exibir informações básicas do objeto veículo
+ * Data: 26/10/2019
+*/
+package br.com.revisao.poo;
+
+public class VeiculoBase {
+	
+	// Atributos
+	private String modelo;
+	private String marca;
+	private String chassi;
+	private int quantidadeRodas;
+	private float quantidadeCombustivel;
+	
+	// Construtor default
+	public VeiculoBase() {
+	}
+	
+	// Métodos assessores get e set
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getChassi() {
+		return chassi;
+	}
+
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+
+	public int getQuantidadeRodas() {
+		return quantidadeRodas;
+	}
+
+	public void setQuantidadeRodas(int quantidadeRodas) {
+		this.quantidadeRodas = quantidadeRodas;
+	}
+
+	public float getQuantidadeCombustivel() {
+		return quantidadeCombustivel;
+	}
+
+	public void setQuantidadeCombustivel(float quantidadeCombustivel) {
+		this.quantidadeCombustivel = quantidadeCombustivel;
+	}
+	
+	// Métodos para ações básicas do veículo
+	public void ligar() {
+		System.out.println("O veículo ligou!");
+	}
+
+	public void desligar() {
+		System.out.println("O veículo desligou!");
+	}
+
+	public void abastecer(float litros) {
+		quantidadeCombustivel += litros;
+	}
+}
