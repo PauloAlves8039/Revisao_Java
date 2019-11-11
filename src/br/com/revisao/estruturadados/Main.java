09d84127b6a8b10e0f5b07286a8bdf5aecebd43c
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-10 23:14:18 -0300

    Adicionando método para exemplificar gerenciamento de memória

--- src/br/com/revisao/estruturadados/Main.java
+++ src/br/com/revisao/estruturadados/Main.java
@@ -9,13 +9,47 @@ package br.com.revisao.estruturadados;
 
 import java.util.Scanner;
 
+import br.com.revisao.estruturadados.modelos.Pessoa;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Digite a opção desejada: ");
+		System.out.println("1. Gerenciamento de memória");
+		int opcao = input.nextInt();
+		
+		switch(opcao) {
+		case 1:
+			gerenciamentoMemoria();
+			break;
+		}
+		
 		input.close();
 	}
 	
+	static void gerenciamentoMemoria() {
+		int a = 3;
+		System.out.println(a);
+		int b = a;
+		System.out.println(b);
+		b = 2;
+		System.out.println("--------------------");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a == b);
+		System.out.println("********************");
+		Pessoa p1 = new Pessoa(1, "Paulo");
+		System.out.println(p1.toString());
+		Pessoa p2 = p1;
+		System.out.println(p2.toString());
+		System.out.println("--------------------");
+		p2.setNome("Pedro");
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		System.out.println(p1 == p2);
+	}
+
 }
