d6e142f15e3771a6dc5e79fa9147c68ab6ebdf8b
PauloAlves8039 <pj.alves547@gmail.com>
2019-11-10 22:31:08 -0300

    Adicionando pacote para estrutura de dados

---
+++ src/br/com/revisao/estruturadados/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Arquivo: Classe - Main
+ * Autor: Paulo Alves
+ * Descrição: utilizada como entry point(ponto de entrada) para execução da aplicação. 
+ * Data: 10/11/2019
+*/
+
+package br.com.revisao.estruturadados;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
