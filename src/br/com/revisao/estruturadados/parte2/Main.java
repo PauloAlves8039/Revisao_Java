4bc2eb119caad2fa5be0354ffe0974d58c37da10
PauloAlves8039 <pj.alves547@gmail.com>
2019-12-20 15:14:17 -0300

    Adicionando segundo pacote de estrutura de dados

---
+++ src/br/com/revisao/estruturadados/parte2/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Arquivo: Classe - Main
+ * Autor: Paulo Alves
+ * Descrição: utilizada como entry point(ponto de entrada) para execução da aplicação. 
+ * Data: 20/12/2019
+*/
+
+package br.com.revisao.estruturadados.parte2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
