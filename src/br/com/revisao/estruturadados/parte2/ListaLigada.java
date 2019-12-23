8cded64908ef3ef2c36930f042f7af9408c2eb40
PauloAlves8039 <pj.alves547@gmail.com>
2019-12-23 16:23:43 -0300

    Métodos para inserção e recuperação de elementos

--- src/br/com/revisao/estruturadados/parte2/ListaLigada.java
+++ src/br/com/revisao/estruturadados/parte2/ListaLigada.java
@@ -8,7 +8,7 @@
 package br.com.revisao.estruturadados.parte2;
 
 public class ListaLigada<T> {
-	
+
 	private No<T> primeiroNo;
 	private No<T> ultimoNo;
 	private int tamanho;
@@ -18,45 +18,91 @@ public class ListaLigada<T> {
 		this.ultimoNo = null;
 		this.tamanho = 0;
 	}
-	
+
 	public void inserir(T elemento) {
 		No<T> novoNo = new No<T>(elemento);
-		if(estaVazia()) {
+		if (estaVazia()) {
 			this.primeiroNo = novoNo;
 			this.ultimoNo = novoNo;
-		}else{
+		} else {
 			this.ultimoNo.setProximo(novoNo);
 			this.ultimoNo = novoNo;
 		}
 		this.tamanho++;
 	}
-	
+
+	public void inserirEm(int posicao, T elemento) {
+		if (posicao >= tamanho()) {
+			throw new IllegalArgumentException(String.format("Posição inválida [%d]", posicao));
+		}
+		if (posicao == 0) {
+			No<T> novoNo = new No<T>(elemento);
+			novoNo.setProximo(this.primeiroNo);
+			this.primeiroNo = novoNo;
+		} else if (posicao == this.tamanho() - 1) {
+			No<T> novoNo = new No<T>(elemento);
+			this.ultimoNo.setProximo(novoNo);
+			this.ultimoNo = novoNo;
+		} else {
+			No<T> noAnterior = recuperarNo(posicao - 1);
+			No<T> noAtual = recuperarNo(posicao);
+			No<T> novoNo = new No<>(elemento);
+			noAnterior.setProximo(novoNo);
+			novoNo.setProximo(noAtual);
+		}
+		this.tamanho++;
+	}
+
+	public T recuperar(int posicao) {
+		No<T> no = recuperarNo(posicao);
+		if (no != null) {
+			return no.getElemento();
+		}
+		return null;
+	}
+
 	public boolean estaVazia() {
 		return this.tamanho == 0;
 	}
-	
+
 	public int tamanho() {
 		return this.tamanho;
 	}
 
+	private No<T> recuperarNo(int posicao) {
+		if (posicao >= tamanho()) {
+			throw new IllegalArgumentException(String.format("Posição inválida [%d]", posicao));
+		}
+		No<T> resultado = null;
+		for (int i = 0; i <= posicao; i++) {
+			if (i == 0) {
+				resultado = this.primeiroNo;
+			} else {
+				resultado = resultado.getProximo();
+			}
+		}
+		return resultado;
+	}
+
 	@Override
 	public String toString() {
-		if(estaVazia()) {
+		if (estaVazia()) {
 			return "Lista []";
-		}else {
+		} else {
 			No<T> noAtual = this.primeiroNo;
 			StringBuilder sb = new StringBuilder();
 			sb.append("List [");
 			sb.append(noAtual.getElemento() != null ? noAtual.getElemento().toString() : "<NULO>");
 			sb.append(",");
-			while(noAtual.getProximo() != null) {
-				sb.append(noAtual.getProximo().getElemento() != null ? noAtual.getProximo().getElemento().toString() : "<NULO>");
+			while (noAtual.getProximo() != null) {
+				sb.append(noAtual.getProximo().getElemento() != null ? noAtual.getProximo().getElemento().toString()
+						: "<NULO>");
 				sb.append(",");
 				noAtual = noAtual.getProximo();
 			}
 			sb.append("}");
 			return sb.toString();
 		}
-		
+
 	}
 }
