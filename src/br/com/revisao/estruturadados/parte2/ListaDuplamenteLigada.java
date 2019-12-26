8187fb351a46c6816991abc9642a31e18004e0e7
PauloAlves8039 <pj.alves547@gmail.com>
2019-12-26 20:34:37 -0300

    Adicionando propriedades na classe ListaDuplamenteLigada

--- src/br/com/revisao/estruturadados/parte2/ListaDuplamenteLigada.java
+++ src/br/com/revisao/estruturadados/parte2/ListaDuplamenteLigada.java
@@ -1,5 +1,21 @@
+/*
+ * Arquivo: Classe - ListaDuplamenteLigada
+ * Autor: Paulo Alves
+ * Descrição: responsável pela manipulação dos elementos da lista duplamente ligada
+ * Data: 26/12/2019
+*/
+
 package br.com.revisao.estruturadados.parte2;
 
-public class ListaDuplamenteLigada {
-
+public class ListaDuplamenteLigada<T> {
+	
+	private No<T> primeiroNo;
+	private No<T> ultimoNo;
+	private int tamanho;
+	
+	public ListaDuplamenteLigada() {
+		this.primeiroNo = null;
+		this.ultimoNo = null;
+		this.tamanho = 0;
+	}
 }
