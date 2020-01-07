2f438ac3e1699583bda5086f687bbf98332af09b
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-07 11:52:07 -0300

    Criando a classe Pilha

---
+++ src/br/com/revisao/estruturadados/parte3/Pilha.java
@@ -0,0 +1,25 @@
+/*
+ * Arquivo: Classe - Pilha
+ * Autor: Paulo Alves
+ * Descrição: responsável pelas funcionalidades de empilhamento e verificação de nós
+ * Data: 07/01/2020
+*/
+
+package br.com.revisao.estruturadados.parte3;
+
+import br.com.revisao.estruturadados.parte2.ListaDuplamenteLigada;
+
+public class Pilha<T> {
+	
+	private ListaDuplamenteLigada<T> elementos;
+	
+	public Pilha() {
+		this.elementos = new ListaDuplamenteLigada<T>();
+	}
+	
+	public boolean estaVazia() {
+		return this.elementos.estaVazia();
+	}
+	
+	
+}
