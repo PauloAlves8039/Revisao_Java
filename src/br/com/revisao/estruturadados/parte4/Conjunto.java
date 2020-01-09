c12b18d552bd537423d24cadd07fb4d1d6953da4
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-08 22:15:53 -0300

    Adicionando lógica de inserção para o Conjunto

--- src/br/com/revisao/estruturadados/parte4/Conjunto.java
+++ src/br/com/revisao/estruturadados/parte4/Conjunto.java
@@ -10,10 +10,38 @@ package br.com.revisao.estruturadados.parte4;
 import br.com.revisao.estruturadados.parte2.ListaLigada;
 
 public class Conjunto<T> {
-	
+
 	private ListaLigada<T> elementos;
-	
+
 	public Conjunto() {
 		this.elementos = new ListaLigada<T>();
+	}
+
+	public boolean inserir(T elemento) {
+		if (elemento != null && !this.contem(elemento)) {
+			this.elementos.inserir(elemento);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean inserirEm(int posicao, T elemento) {
+		if (elemento != null && !this.contem(elemento)) {
+			this.elementos.inserirEm(posicao, elemento);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean estaVazio() {
+		return this.elementos.estaVazia();
+	}
+
+	public int tamanho() {
+		return this.elementos.tamanho();
+	}
+
+	public boolean contem(T elemento) {
+		return this.elementos.contem(elemento);
 	}
 }
