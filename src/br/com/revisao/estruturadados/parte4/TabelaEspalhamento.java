8ff094b53b2e484060cd240ff6da6617264f73f5
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-17 20:44:19 -0300

    Refatorando métodos

--- src/br/com/revisao/estruturadados/parte4/TabelaEspalhamento.java
+++ src/br/com/revisao/estruturadados/parte4/TabelaEspalhamento.java
@@ -24,11 +24,11 @@ public class TabelaEspalhamento<T> {
 	}
 	
 	public boolean inserir(T elemento) {
-		int numeroEspalhamento = this.gerarNumeroEspalhamento(elemento);
-		ListaLigada<T> categoria = this.elementos.recuperar(numeroEspalhamento);
-		if(categoria.contem(elemento)) {
+		if(this.contem(elemento)) {
 			return false;
 		}
+		int numeroEspalhamento = this.gerarNumeroEspalhamento(elemento);
+		ListaLigada<T> categoria = this.elementos.recuperar(numeroEspalhamento);
 		categoria.inserir(elemento);
 		this.tamanho++;
 		return true;
@@ -45,7 +45,18 @@ public class TabelaEspalhamento<T> {
 		return this.tamanho;
 	}
 	
+	public boolean contem(T elemento) {
+		int numeroEspalhamento = this.gerarNumeroEspalhamento(elemento);
+		ListaLigada<T> categoria = this.elementos.recuperar(numeroEspalhamento);
+		return categoria.contem(elemento);
+	}
+	
 	private int gerarNumeroEspalhamento(T elemento) {
-		return elemento.hashCode() % 16;
+		return elemento.hashCode() % this.numeroCategorias;
+	}
+
+	@Override
+	public String toString() {
+		return "TabelaEspalhamento [elementos=" + elementos + "]";
 	}
 }
