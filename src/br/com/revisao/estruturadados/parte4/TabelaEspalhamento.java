43414f4e8abb96db6c1445e03537267a79815d78
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-17 20:36:31 -0300

    Lógica para remoção e verificação de elementos

--- src/br/com/revisao/estruturadados/parte4/TabelaEspalhamento.java
+++ src/br/com/revisao/estruturadados/parte4/TabelaEspalhamento.java
@@ -13,12 +13,14 @@ public class TabelaEspalhamento<T> {
 	
 	private ListaLigada<ListaLigada<T>> elementos;
 	private int numeroCategorias = 16;
+	private int tamanho;
 	
 	public TabelaEspalhamento() {
 		this.elementos = new ListaLigada<ListaLigada<T>>();
 		for(int i = 0; i < this.numeroCategorias; i++) {
 			this.elementos.inserir(new ListaLigada<T>());
 		}
+		this.tamanho = 0;
 	}
 	
 	public boolean inserir(T elemento) {
@@ -28,7 +30,19 @@ public class TabelaEspalhamento<T> {
 			return false;
 		}
 		categoria.inserir(elemento);
+		this.tamanho++;
 		return true;
+	}
+	
+	public void remover(T elemento) {
+		int numeroEspalhamento = this.gerarNumeroEspalhamento(elemento);
+		ListaLigada<T> categoria = this.elementos.recuperar(numeroEspalhamento);
+		categoria.remover(elemento);
+		this.tamanho--;
+	}
+	
+	public int tamanho() {
+		return this.tamanho;
 	}
 	
 	private int gerarNumeroEspalhamento(T elemento) {
