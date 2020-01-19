eb0714a0bfb5e079b23f4cdb9b0ff36d07f547e6
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-19 16:10:39 -0300

    Criando a classe Arvore

---
+++ src/br/com/revisao/estruturadados/parte4/Arvore.java
@@ -0,0 +1,47 @@
+/*
+ * Arquivo: Classe - Arvore
+ * Autor: Paulo Alves
+ * Descrição: responsável por representar uma arvore binária 
+ * Data: 19/01/2020
+*/
+
+package br.com.revisao.estruturadados.parte4;
+
+public class Arvore<T> {
+
+	private NoArvore<T> raiz;
+
+	public Arvore() {
+		this.raiz = null;
+	}
+
+	public NoArvore<T> getRaiz() {
+		return this.raiz;
+	}
+
+	public void inserir(NoArvore<T> no) {
+		no.setNoDireito(null);
+		no.setNoEsquerdo(null);
+		if (this.raiz == null) {
+			this.raiz = no;
+		} else {
+			this.inserir(this.raiz, no);
+		}
+	}
+
+	private void inserir(NoArvore<T> ref, NoArvore<T> novoNo) {
+		if (ref.peso() < novoNo.peso()) {
+			if (ref.getNoDireito() == null) {
+				ref.setNoDireito(novoNo);
+			} else {
+				inserir(ref.getNoDireito(), novoNo);
+			}
+		} else {
+			if (ref.getNoEsquerdo() == null) {
+				ref.setNoEsquerdo(novoNo);
+			} else {
+				inserir(ref.getNoEsquerdo(), novoNo);
+			}
+		}
+	}
+}
