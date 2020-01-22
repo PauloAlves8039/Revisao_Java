62f803946273c3dad9e353fbb048640d490bb516
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-22 11:41:00 -0300

    Adicionando package para uso de coleções

---
+++ src/br/com/revisao/collections/parte1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Arquivo: Classe - Main
+ * Autor: Paulo Alves
+ * Descrição: utilizada como entry point(ponto de entrada) para execução da aplicação. 
+ * Data: 22/01/2020
+*/
+
+package br.com.revisao.collections.parte1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
