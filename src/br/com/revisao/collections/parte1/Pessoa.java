e2144c0fb7517db1ff0c670ec8c6e4c252a844ac
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-23 12:38:33 -0300

    Adicionando a classe de modelo Pessoa

---
+++ src/br/com/revisao/collections/parte1/Pessoa.java
@@ -0,0 +1,31 @@
+/*
+ * Arquivo: Classe - Pessoa
+ * Autor: Paulo Alves
+ * Descrição: responsável por exibir informações básicas do objeto pessoa. 
+ * Data: 23/01/2020
+*/
+
+package br.com.revisao.collections.parte1;
+
+public class Pessoa {
+
+	private int id;
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
